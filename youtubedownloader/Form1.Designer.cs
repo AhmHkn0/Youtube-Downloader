d19ba1b8c25a305880c8c714d7ba53bc4d84f514
Ahmet Hakan <ahmhakan16@gmail.com>
2022-06-25 02:16:42 +0300

    +

---
+++ youtubedownloader/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace youtubedownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.video_adi_block = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.video_cozunurluk = new System.Windows.Forms.Label();
+            this.video_adi = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "İndir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(398, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(273, 109);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 19);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ses/MP3";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(467, 109);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(84, 19);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Video/MP4";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(145, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(527, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "İndirmek istediğiniz videonun linkini buraya yapıştırın.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "İndirilenler klasörünü aç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(67, 97);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(447, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // video_adi_block
+            // 
+            this.video_adi_block.AutoSize = true;
+            this.video_adi_block.BackColor = System.Drawing.Color.Transparent;
+            this.video_adi_block.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.video_adi_block.Location = new System.Drawing.Point(0, 4);
+            this.video_adi_block.Name = "video_adi_block";
+            this.video_adi_block.Size = new System.Drawing.Size(80, 21);
+            this.video_adi_block.TabIndex = 7;
+            this.video_adi_block.Text = "Video Adı:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.video_cozunurluk);
+            this.panel1.Controls.Add(this.video_adi);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.video_adi_block);
+            this.panel1.Location = new System.Drawing.Point(206, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(492, 70);
+            this.panel1.TabIndex = 8;
+            // 
+            // video_cozunurluk
+            // 
+            this.video_cozunurluk.AutoSize = true;
+            this.video_cozunurluk.BackColor = System.Drawing.Color.Transparent;
+            this.video_cozunurluk.Font = new System.Drawing.Font("Franklin Gothic Heavy", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.video_cozunurluk.Location = new System.Drawing.Point(96, 43);
+            this.video_cozunurluk.Name = "video_cozunurluk";
+            this.video_cozunurluk.Size = new System.Drawing.Size(70, 16);
+            this.video_cozunurluk.TabIndex = 11;
+            this.video_cozunurluk.Text = "cozunurluk";
+            // 
+            // video_adi
+            // 
+            this.video_adi.AutoSize = true;
+            this.video_adi.BackColor = System.Drawing.Color.Transparent;
+            this.video_adi.Font = new System.Drawing.Font("Franklin Gothic Heavy", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.video_adi.Location = new System.Drawing.Point(86, 9);
+            this.video_adi.Name = "video_adi";
+            this.video_adi.Size = new System.Drawing.Size(54, 16);
+            this.video_adi.TabIndex = 10;
+            this.video_adi.Text = "videoadi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(0, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Çözünürlük:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(171, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 64);
+            this.listBox1.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(604, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 38);
+            this.button3.TabIndex = 10;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.progressBar1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(159, 323);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(586, 192);
+            this.panel2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "İndirmek istediğiniz çözünürlüğü seçin.";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(200, 153);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(447, 23);
+            this.progressBar2.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.progressBar2);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.radioButton1);
+            this.panel3.Controls.Add(this.radioButton2);
+            this.panel3.Location = new System.Drawing.Point(26, 88);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(829, 229);
+            this.panel3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 527);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Youtube Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label1;
+        private Button button2;
+        private ProgressBar progressBar1;
+        private Label video_adi_block;
+        private Panel panel1;
+        private Label video_cozunurluk;
+        private Label video_adi;
+        private Label label3;
+        private ListBox listBox1;
+        private Button button3;
+        private Panel panel2;
+        private Label label4;
+        private ProgressBar progressBar2;
+        private Panel panel3;
+    }
+}
---
+++ youtubedownloader/Form1.cs
@@ -0,0 +1,245 @@
+namespace youtubedownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        List<VideoLibrary.YouTubeVideo> videos = new List<VideoLibrary.YouTubeVideo>();
+
+        VideoLibrary.YouTubeVideo video;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            konum();
+            radioButton1.Checked = true;
+            video_adi.Text = "";
+            video_cozunurluk.Text = "";
+
+            panel2.Visible = false;
+
+            progressBar2.Visible = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bilgilerisifirla();
+            listBox1.Items.Clear();
+            string link = textBox1.Text;
+            if (radioButton1.Checked == true)
+                sesindir(link);
+            if (radioButton2.Checked == true)
+                videocek(link);
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                foreach (VideoLibrary.YouTubeVideo video in videos)
+                {
+                    string res = video.Resolution.ToString();
+                    if (listBox1.SelectedItem.ToString() == res)
+                    {
+                        videoindir(video);
+                        bilgileriyazdir(video.FullName.ToString(), video.Resolution.ToString()+"P");
+                        break;
+                    }
+                }
+                }
+            else
+            {
+                MessageBox.Show("Lütfen indirmek istediðiniz çözünürlüðü listeden seçin.");
+            }
+            
+            
+            
+        }
+
+        
+
+        private async void videocek(String link)
+        {
+            progressBar2.Visible = false;
+
+            try
+            {
+                
+                var youTube = VideoLibrary.YouTube.Default;
+                foreach (VideoLibrary.YouTubeVideo video in youTube.GetAllVideos(link))
+                {
+                    videos.Add(video);
+                    string res = video.Resolution.ToString();
+                    if (!listBox1.Items.Contains(res) && res != "-1")
+                    {
+                        listBox1.Items.Add(res);
+                    }
+
+                    if (video_adi.Text == "")
+                    {
+                        bilgileriyazdir(video.FullName.ToString(), "-");
+                    }
+                }
+
+                listBox1.Sorted = true;
+
+                panel1.Visible = true;
+                panel2.Visible = true;
+
+            }
+            catch
+            {
+                MessageBox.Show("Yazdýðýnýz link hatalý ya da video indirilemiyor.", "Hata");
+            }
+            
+        }
+
+        private async void videoindir(VideoLibrary.Video video)
+        {
+            progressBar1.Value = 30;
+            if (listBox1.SelectedItem != null)
+            {
+                await Task.Run(() =>
+                {
+                    File.WriteAllBytes(indirmekonumu() + video.FullName, video.GetBytes());
+                });
+
+                progressBar1.Value = 100;
+                MessageBox.Show("Ýndirme tamamlandý.", "Video indirildi");
+                Thread.Sleep(100);
+                progressBar1.Value = 0;
+            }
+
+            
+        }
+
+
+        private async void sesindir(String link)
+        {
+            
+            try
+            {
+
+                var youTube = VideoLibrary.YouTube.Default;
+                video = youTube.GetVideo(link);
+                panel1.Visible = true;
+                progressBar2.Visible = true;
+                progressBar2.Value = 20;
+                bilgileriyazdir(video.FullName.ToString(), "128Kbps");
+                await Task.Run(() =>
+                {
+                    File.WriteAllBytes(indirmekonumu() + video.FullName, video.GetBytes());
+                });
+
+                progressBar2.Value = 80;
+
+                Mp4ToMp3(indirmekonumu() + "\\" + video.FullName);
+
+                progressBar2.Value = 100;
+                MessageBox.Show("Ýndirme tamamlandý.", "Video indirildi");
+                Thread.Sleep(100);
+                progressBar2.Value = 0;
+                progressBar2.Visible = false;
+                
+            }
+            catch
+            {
+                MessageBox.Show("Yazdýðýnýz link hatalý ya da video indirilemiyor.", "Hata");
+            }
+
+            
+
+        }
+
+        
+
+        private void Mp4ToMp3(String path)
+        {
+            var inputFile = new MediaToolkit.Model.MediaFile { Filename = path };
+            var outputFile = new MediaToolkit.Model.MediaFile { Filename = $"{path}.mp3" };
+
+            using (var engine = new MediaToolkit.Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                engine.Convert(inputFile, outputFile);
+
+                File.Delete(Path.Combine(path));
+            }
+        }
+
+        private void bilgileriyazdir(String baslik, String cozunurluk)
+        {
+            video_adi.Text = baslik;
+            video_cozunurluk.Text = cozunurluk;
+        }
+
+        private void bilgilerisifirla()
+        {
+            video_adi.Text = "";
+            video_cozunurluk.Text = "";
+        }
+        private void konum()
+        {
+            
+            String path = konumcek();
+            System.IO.Directory.CreateDirectory(path);
+        }
+
+        private String konumcek()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\YoutubeDownloader";
+        }
+
+        private String indirmekonumu()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\YoutubeDownloader\\";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            indirmekonumuac();
+        }
+        private void indirmekonumuac()
+        {
+            System.Diagnostics.Process.Start("explorer.exe", indirmekonumu());
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == false)
+                radioButton2.Checked = true;
+            if (radioButton1.Checked == true)
+            {
+                radioButton2.Checked = false;
+                if (panel2.Visible == true)
+                    panel2.Visible = false;
+            }
+                
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == false)
+                radioButton1.Checked = true;
+            if (radioButton2.Checked == true)
+                radioButton1.Checked = false;
+        }
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                button3.PerformClick();
+            }
+        }
+
+
+    }
+}
